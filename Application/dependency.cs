a75e906ba01e77ae139577e11902ce40bca7a0ab
Amirabbas Taherian <aa.taherian@gmail.com>
2023-03-15 11:37:16 +0000

    Update Application Project Configurations

--- Application/dependency.cs
+++ Application/dependency.cs
@@ -1,4 +1,8 @@
 using Microsoft.Extensions.DependencyInjection;
+using AutoMapper;
+
+using ToDoApplication.Application.DTOs;
+using ToDoApplication.Core.Entities;
 
 namespace ToDoApplication.Application;
 
@@ -6,6 +10,14 @@ public static class Dependency{
 
     public static void ConfigureApplication(this IServiceCollection services)
     {
+        var mapperConfig = new MapperConfiguration(
+            config =>
+            {
+                config.CreateMap<Card,CardDTO>().ReverseMap();
+                config.CreateMap<Card,CardCreationDTO>().ReverseMap();
+            }
+        );
+        var mapper = mapperConfig.CreateMapper();
         return;
     }
 
